e3886a9e3198067933a52dea1b58bf259d622868
Arnav Srivastava <arnavsrivastava017@gmail.com>
2018-09-02 03:04:44 +0530

    correcting id error

--- app/src/main/java/com/example/arnav/ecellapplication/MainActivity.java
+++ app/src/main/java/com/example/arnav/ecellapplication/MainActivity.java
@@ -94,7 +94,7 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_share) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.enter) {
 
         }
 
