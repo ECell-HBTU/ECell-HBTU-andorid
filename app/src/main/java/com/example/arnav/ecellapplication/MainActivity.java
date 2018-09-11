d893a9a658d76a5824ae0494c0285b17adb788a2
Arnav Srivastava <arnavsrivastava017@gmail.com>
2018-09-11 13:48:10 +0530

    first

--- app/src/main/java/com/example/arnav/ecellapplication/MainActivity.java
+++ app/src/main/java/com/example/arnav/ecellapplication/MainActivity.java
@@ -22,6 +22,9 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+        
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
