a3cdf188e228159251e4cb9ca125516ed4e40800
Amol <amol6022@gmail.com>
2018-09-11 20:07:56 +0530

    Added new activity which shows the list of events.

--- app/src/main/java/com/example/arnav/ecellapplication/MainActivity.java
+++ app/src/main/java/com/example/arnav/ecellapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.arnav.ecellapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -44,10 +45,6 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
-        int x=5;
-
-        System.out.println(x);
     }
 
     @Override
@@ -91,7 +88,8 @@ public class MainActivity extends AppCompatActivity
         if (id == R.id.nav_camera) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
-
+            Intent intent = new Intent(this,EventsActivity.class);
+            startActivity(intent);
         } else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
