c24cd0c37b9f661312402dffb4ca745096241aa7
Arnav Srivastava <arnavsrivastava017@gmail.com>
2018-09-12 04:19:15 +0530

    swipe view added again

---
+++ app/src/main/java/com/example/arnav/ecellapplication/swipeView/internal/CardStackOption.java
@@ -0,0 +1,24 @@
+package com.example.arnav.ecellapplication.swipeView.internal;
+
+
+
+import com.example.arnav.ecellapplication.swipeView.StackFrom;
+import com.example.arnav.ecellapplication.swipeView.SwipeDirection;
+
+
+import java.util.List;
+
+public class CardStackOption {
+    public int visibleCount = 3;
+    public float swipeThreshold = 0.75f; // Percentage
+    public float translationDiff = 12f; // DP
+    public float scaleDiff = 0.02f; // Percentage
+    public StackFrom stackFrom = StackFrom.DEFAULT;
+    public boolean isElevationEnabled = true;
+    public boolean isSwipeEnabled = true;
+    public int leftOverlay = 0; // Layout Resource ID
+    public int rightOverlay = 0; // Layout Resource ID
+    public int bottomOverlay = 0; // Layout Resource ID
+    public int topOverlay = 0; // Layout Resource ID
+    public List<SwipeDirection> swipeDirection = SwipeDirection.FREEDOM;
+}
