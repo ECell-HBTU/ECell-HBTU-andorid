ca1fdeab2645df30889ff271b5c353465d461034
Amol <amol6022@gmail.com>
2018-09-12 20:00:39 +0530

    Added EventDetail Activity. The size or the scaleType of the "event image" in this activity might need to be changed.

---
+++ app/src/main/java/com/example/arnav/ecellapplication/EventDetailActivity.java
@@ -0,0 +1,37 @@
+package com.example.arnav.ecellapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class EventDetailActivity extends AppCompatActivity {
+    private String eventTitle=null;
+    private int eventImageId;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_detail);
+
+        Bundle extras=getIntent().getExtras();
+
+        if(extras!=null){
+            eventTitle=extras.getString("EVENT_TITLE");
+            eventImageId=extras.getInt("EVENT_IMAGE_ID");
+        }
+        System.out.println("idddd: "+eventImageId);
+        TextView title=(TextView)findViewById(R.id.event_title);
+        ImageView image=(ImageView)findViewById(R.id.event_image);
+
+        title.setText(eventTitle);
+        image.setBackground(getResources().getDrawable(eventImageId));
+
+    }
+
+    public void backToEventsActivity(View v){
+        Intent intent=new Intent(this,EventsActivity.class);
+        startActivity(intent);
+    }
+}
--- app/src/main/java/com/example/arnav/ecellapplication/RViewAdapter.java
+++ app/src/main/java/com/example/arnav/ecellapplication/RViewAdapter.java
@@ -1,8 +1,12 @@
 package com.example.arnav.ecellapplication;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +16,16 @@ import android.widget.TextView;
 import java.util.List;
 
 public class RViewAdapter extends RecyclerView.Adapter<RViewAdapter.EventViewHolder> {
+    private OnItemClickListener myListener;
+
+    public interface OnItemClickListener{
+        void onItemClick(int position);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener){
+        myListener=listener;
+    }
+
     public static class EventViewHolder extends RecyclerView.ViewHolder{
         CardView CView;
         TextView eventTitle;
@@ -19,20 +33,34 @@ public class RViewAdapter extends RecyclerView.Adapter<RViewAdapter.EventViewHol
         TextView eventTiming;
         ImageView eventImage;
 
-        EventViewHolder(View itemView){
+        EventViewHolder(View itemView,final OnItemClickListener listener){
             super(itemView);                        //what does this constructor do and how are we able to call fvbi on a view object
-            CView= itemView.findViewById(R.id.CView);
+
             eventTitle= itemView.findViewById(R.id.event_title);
             eventDate= itemView.findViewById(R.id.event_date);
             eventTiming= itemView.findViewById(R.id.event_timing);
             eventImage= itemView.findViewById(R.id.event_image);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Log.i("MY_LOG","ON CLICK WORKS!");
+
+                    Intent intent=new Intent(current,EventDetailActivity.class);
+                    intent.putExtra("EVENT_IMAGE_ID",(Integer)eventImage.getTag());
+                    intent.putExtra("EVENT_TITLE",eventTitle.getText());
+                    current.startActivity(intent);
+
+                }
+            });
         }
     }
-
+    private static EventsActivity current;
     private List<Event> events;
 
-    RViewAdapter(List<Event> events){
+    RViewAdapter(List<Event> events,EventsActivity current){
         this.events=events;
+        RViewAdapter.current=current;
     }
 
     public int getItemCount(){
@@ -43,7 +71,7 @@ public class RViewAdapter extends RecyclerView.Adapter<RViewAdapter.EventViewHol
     @Override
     public EventViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v= LayoutInflater.from(parent.getContext()).inflate(R.layout.event_card,parent,false);
-        EventViewHolder evh=new EventViewHolder(v);
+        EventViewHolder evh=new EventViewHolder(v,myListener);
         return evh;
     }
 
@@ -53,6 +81,6 @@ public class RViewAdapter extends RecyclerView.Adapter<RViewAdapter.EventViewHol
         holder.eventDate.setText(events.get(position).event_date);
         holder.eventTiming.setText(events.get(position).event_timing);
         holder.eventImage.setImageResource(events.get(position).eventImageId);
-
+        holder.eventImage.setTag(events.get(position).eventImageId);
     }
 }
