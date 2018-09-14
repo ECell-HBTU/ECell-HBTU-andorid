75901542e41cefb6821f8f50f8a4c121efc9b457
Amol <amol6022@gmail.com>
2018-09-14 06:03:54 +0530

    Events Images Added. Main Page Filled with crap.

--- app/src/main/java/com/example/arnav/ecellapplication/ArchivesActivity.java
+++ app/src/main/java/com/example/arnav/ecellapplication/ArchivesActivity.java
@@ -21,15 +21,18 @@ public class ArchivesActivity extends AppCompatActivity{
         List<Event> events = new ArrayList<>();
 
         events.add(new Event("Startup Boosting Workshop","1st Sept, 2018","11AM to 1PM",R.mipmap.archive7));
-        events.add(new Event("Talk on Waste Management and start-ups Q/A session","23rd Oct, 2018","5PM to 6PM",R.mipmap.archive8));
-        events.add(new Event("How to gain traction in the initial months?","11th Sept, 2018","10AM to 11AM",R.mipmap.sample2));
-        events.add(new Event("Pitching Workshop","12th Sept, 2018","10AM to 1PM",R.mipmap.sample3));
-        events.add(new Event("Elevator Pitch","12th Sept, 2018","10AM to 1PM",R.mipmap.sample4));
-        events.add(new Event("Start-up 101","12th Sept, 2018","10AM to 1PM",R.mipmap.sample1));
-        events.add(new Event("Talk : 10 ways to gain investors attention!","12th Sept, 2018","10AM to 1PM",R.mipmap.sample3));
-        events.add(new Event("Innovative Idea Competition","12th Sept, 2018","10AM to 1PM",R.mipmap.sample2));
-        events.add(new Event("Start-up quiz","12th Sept, 2018","10AM to 1PM",R.mipmap.sample4));
-        events.add(new Event("Talk:Things to keep in mind before pivoting","12th Sept, 2018","10AM to 1PM",R.drawable.ic_launcher));
+        events.add(new Event("Talk on Waste Management and start-ups Q/A session","23rd Oct, 2018","5PM to 6PM",R.mipmap.archive9));
+        events.add(new Event("How to gain traction in the initial months?","11th Sept, 2018","10AM to 11AM",R.mipmap.archive2));
+        events.add(new Event("Pitching Workshop","12th Sept, 2018","10AM to 1PM",R.mipmap.archive3));
+        events.add(new Event("Elevator Pitch","12th Sept, 2018","10AM to 1PM",R.mipmap.archive4));
+        events.add(new Event("Start-up 101","12th Sept, 2018","10AM to 1PM",R.mipmap.archive8));
+        events.add(new Event("Talk : 10 ways to gain investors attention!","12th Sept, 2018","10AM to 1PM",R.mipmap.archive10));
+        events.add(new Event("Innovative Idea Competition","12th Sept, 2018","10AM to 1PM",R.mipmap.archive12));
+        events.add(new Event("Start-up quiz","12th Sept, 2018","10AM to 1PM",R.mipmap.archive5));
+        events.add(new Event("Talk:Things to keep in mind before pivoting","12th Sept, 2018","10AM to 1PM",R.mipmap.archive11));
+        events.add(new Event("Startup Boosting Workshop","1st Sept, 2018","11AM to 1PM",R.mipmap.archive7));
+        events.add(new Event("Talk on Waste Management and start-ups Q/A session","23rd Oct, 2018","5PM to 6PM",R.mipmap.archive9));
+        events.add(new Event("How to gain traction in the initial months?","11th Sept, 2018","10AM to 11AM",R.mipmap.archive2));
 
         ArchiveImageAdapter adapter = new ArchiveImageAdapter(events);
         StaggeredGridLayoutManager manager = new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL);
